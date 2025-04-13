5203e4465b73f6f65d479d9e0781dd01004e62c0
Dartanhan Ramos <alessandroramosd@gmail.com>
2025-04-13 15:16:26 -0400

    Completed exer 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,34 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(
+  first_name: "Khurram", 
+  last_name: "Virani", 
+  hourly_rate: 60)
+
+@store1.employees.create(
+  first_name: "Dro", 
+  last_name: "Ramos", 
+  hourly_rate: 100)
+
+@store1.employees.create(
+  first_name: "Simon", 
+  last_name: "Vergara", 
+  hourly_rate: 95)
+
+@store2.employees.create(
+  first_name: "Fidel", 
+  last_name: "Romaro", 
+  hourly_rate: 75)
+
+@store2.employees.create(
+  first_name: "Jenees", 
+  last_name: "Romero", 
+  hourly_rate: 120)
+  
+@store2.employees.create(
+  first_name: "Khurram", 
+  last_name: "Virani", 
+  hourly_rate: 60)
+
+  
