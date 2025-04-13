be17cdb73e149ebb26f518f2333cc4013ec54fc1
Dartanhan Ramos <alessandroramosd@gmail.com>
2025-04-13 15:06:19 -0400

    Completed exer 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum(:annual_revenue)
+puts "Total company revenue: $#{@total_revenue}"
+
+@store_avg = Store.average(:annual_revenue)
+puts "Average store revenue: $#{@store_avg}"
+
+@high_revenue_stores_count = Store.where("annual_revenue > ?", 1000000).count
+puts "Number of stores generating $1M or more: #{@high_revenue_stores_count}"
