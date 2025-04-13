90d8ff673037adb625a8e749c44f78fe54fd3235
Dartanhan Ramos <alessandroramosd@gmail.com>
2025-04-13 16:05:14 -0400

    Completed exer 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+print "Enter store name:"
+store_name = gets.chomp
+
+new_store = Store.new(name: store_name)
+
+if new_store.save
+  puts "Store created successfully!"
+else
+  puts "Could not create store. Errors:"
+  new_store.errors.full_messages.each do |message|
+    puts "- #{message}"
+  end
+end
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,11 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { 
+    only_integer: true,
+    in: 40..200
+  }
+  validates :store, presence: true
 end
